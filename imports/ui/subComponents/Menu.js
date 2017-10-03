e70a0184dacbf06fd75954555047410cad7c574a
Stuart Fong <stuartbfong@gmail.com>
2017-10-03 15:41:16 -0400

    fixed navbar preventing clicks

--- imports/ui/subComponents/Menu.js
+++ imports/ui/subComponents/Menu.js
@@ -9,14 +9,13 @@ class Menu extends React.Component{
   extendMenu(){
     let menuItems = document.querySelectorAll(".menu-item");
     for(let i = 0; i <= menuItems.length; i++ ){
-      menuItems[i].classList.toggle("menu-item-extend") 
+      menuItems[i].classList.toggle("menu-item-extend")
     }
-    console.log(menuItems)
   }
   render(){
     return(
-      <div className="center center-v-outer">
-        <div className="menu center-v-inner" ref="menu" onClick={this.extendMenu.bind(this)}>
+      <div className="center">
+        <div className="menu" ref="menu" onClick={this.extendMenu.bind(this)}>
           <h1 className="menu-header">PostNote</h1>
           <p className="menu-item"><Link to="/">Home</Link></p>
           <p className="menu-item"><Link to="/searchNotes">Notes</Link></p>
